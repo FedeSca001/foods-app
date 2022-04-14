301e528be1ca777c2aa4ac9f65c56b40e3c64b60
FedeSca001 <familadore@gmail.com>
2022-04-14 00:13:03 -0300

    add api restorants

--- src/components/ProductList/ProductList.vue
+++ src/components/ProductList/ProductList.vue
@@ -1,25 +1,67 @@
 <template>
   <v-main>
-    <p v-for="resto in restorant" :key="resto.id">{{resto.name}}</p>
+    <h3>
+      {{useUser().name}} welcome to {{useProducts().restorants.length}} restorant
+    </h3>
+    <div class="containFlex">
+      <v-col v-for="resto in useProducts().restorants" :key="resto.id"
+        cols="4"
+        class="column">
+        <v-card
+          class="mx-auto">
+          <v-img
+            :src="resto.avatar"
+            height="250px"
+            cover
+          ></v-img>
+          <v-card-title>
+            {{resto.name}}
+          </v-card-title>
+
+          <v-card-subtitle>
+            {{resto.descp}}
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-btn
+              color="orange-lighten-2"
+              variant="text">
+              Foods list
+            </v-btn>
+          </v-card-actions>
+          <v-card-text v-for="food in resto.foods" :key="food.id" >
+            {{food.foodname}}
+          </v-card-text>
+        </v-card>
+        </v-col>
+      </div>
   </v-main>
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity'
 import {useProducts} from '../../store/products'
+import {useUser} from '../../store/index'
 
   let url = 'https://623b1a4c46a692bd844e72cc.mockapi.io/api/food/vuejs/restorant'
+  const show = ref(false)
   async function getData(){
-     const r = await fetch(url)
-     let restorant = await r.json()
-     useProducts().restorants = restorant
+    const r = await fetch(url)
+    let restorant = await r.json()
+    useProducts().restorants = restorant
   }
   getData()
-  
-  return {
-    restorant
-  }
 </script>
 
-<style>
-
+<style scoped>
+.column{
+    margin: 0 auto;
+    min-height: 20vw;
+}
+.containFlex{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0px;
+    margin: 0;
+    justify-items: center;
+}
 </style>
