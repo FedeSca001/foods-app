203820e3cf7861e17d64f763a4c7fa0821bec6df
FedeSca001 <familadore@gmail.com>
2022-04-21 03:53:46 -0300

    add admim

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
+<section>
+  <SelectSection v-if="administrator.name === client.name"/>
   <ProductList/>
+</section>
 </template>
 
 <script setup>
 import ProductList from '../components/ProductList/ProductList.vue'
+import { useUser } from '../store'
+import SelectSection from '../components/SelectSection/SelectSection.vue'
+
+
+let use = useUser()
+let client = use.userLoged
+let administrator = use.adminAccess
 </script>
 
 <style>
